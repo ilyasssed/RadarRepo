69407b28e1986a68426fde2258c255f082b4865f
Ilyass SEDDOUG <ilyass.seddoug@gmail.com>
2020-11-26 15:37:05 +0100

    2nd

--- Radar.py
+++ Radar.py
@@ -1,7 +1,29 @@
 import pandas as pd
 import numpy as np
 import torch
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("Radar_Traffic_Counts.csv")
 df_new = df.copy()
+
+
+#Transform object data to integer classes
+
+mappings = {}
+
+for i in range(df_new.shape[1]):
+    if df_new.iloc[:,i].dtypes == 'O':
+        labels_list=list(df_new.iloc[:,i].unique())
+        mapping = dict(zip(labels_list,range(len(labels_list))))
+        mappings[df_new.columns[i]] = (mapping)
+        df_new.iloc[:,i] = df_new.iloc[:,i].map(mapping)
+
+
+#divide data into features and labels
+        
+X = df_new.drop("Volume", axis = 1)
+y = df_new.Volume
+
+scaler = StandardScaler()
+X_scaled = scaler.fit(X)
 
