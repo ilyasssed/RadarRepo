84d4a3cd987b55230073b834dcf94d63bca90879
Ilyass SEDDOUG <ilyass.seddoug@gmail.com>
2020-12-10 14:09:09 +0100

    10/12/2020 - 2

--- Radar.py
+++ Radar.py
@@ -222,3 +222,95 @@ for epoch in range(epochs):
     print('epochs {}/{}'.format(epoch+1,epochs))
     Train_huber()
     Test_huber()
+    
+#Now we define a convolutional neural network
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN,self).__init__()
+        self.conv1d = nn.Conv1d(1,32,kernel_size= 3, stride = 1)
+        self.relu = nn.ReLU(inplace = True)
+        self.maxpool = nn.MaxPool1d(3, stride=1)
+        self.fc1 = nn.Linear(7 * 32,50)
+        self.fc2 = nn.Linear(50,1)
+        
+    def forward(self,x):
+        x = self.conv1d(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = x.view(10,-1)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        
+        return x
+
+model_CNN = CNN().to(device)
+optimizer = torch.optim.Adam(model_CNN.parameters(), lr=1e-5)
+criterion = nn.SmoothL1Loss()
+
+train_losses_CNN = []
+
+
+def Train_CNN():
+    
+    running_loss = .0
+    
+    model_CNN.train()
+    
+    for idx, (inputs,labels) in enumerate(train_loader):
+        inputs = inputs.view(-1,1,11)
+        inputs = inputs.to(device)
+        labels = labels.float().to(device)
+        optimizer.zero_grad()
+        preds = model_CNN(inputs.float())
+        loss = criterion(preds,labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss
+        
+    train_loss = running_loss/len(train_loader)
+    train_losses_CNN.append(train_loss.detach().cpu().numpy())
+    
+    print(f'train_loss {train_loss}')
+
+test_losses_CNN = []
+
+def Test_CNN():
+    
+    running_loss = .0
+    
+    model_CNN.eval()
+    
+    with torch.no_grad():
+        for idx, (inputs, labels) in enumerate(test_loader):
+            inputs = inputs.view(-1,1,11)
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()
+            preds = model_CNN(inputs.float())
+            loss = criterion(preds,labels)
+            running_loss += loss
+            
+        test_loss = running_loss/len(test_loader)
+        test_losses_CNN.append(test_loss.detach().cpu().numpy())
+        print(f'test_loss {test_loss}')
+
+epochs = 200
+for epoch in range(epochs):
+    print('epochs {}/{}'.format(epoch+1,epochs))
+    Train_CNN()
+    Test_CNN()
+    
+#Plot the losses
+fig, ax = plt.subplots()
+color = "tab:blue"
+ax.plot(range(len(train_losses_CNN)), train_losses_CNN, color = color, label = "train loss")
+
+color = "tab:red"
+ax.plot(range(len(test_losses_CNN)), test_losses_CNN, color = color, label = "test loss")
+
+legend = ax.legend(loc='upper right', shadow=True, fontsize='x-large')
+
+plt.show
